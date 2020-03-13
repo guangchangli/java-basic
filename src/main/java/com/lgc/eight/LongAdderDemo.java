530e168857a3e6b72feb3350df5dbea2076b06f1
guangchangli <guangchangde@gmail.com>
2020-03-13 23:58:22 +0800

    add 'LongAddr'

--- src/main/java/com/lgc/eight/LongAdderDemo.java
+++ src/main/java/com/lgc/eight/LongAdderDemo.java
@@ -1,10 +1,16 @@
 package com.lgc.eight;
 
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+
 /**
+ * 高并发下统计
  * @author lgc
  **/
 public class LongAdderDemo {
     public static void main(String[] args) {
-
+        LongAdder longAdder = new LongAdder();
+        AtomicLong atomicLong = new AtomicLong();
+//        Lock lock = new Lock();
     }
 }
