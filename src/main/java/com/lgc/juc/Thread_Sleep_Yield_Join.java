46e44c219151df8e3fa5f16801b7bbadac9a9d74
guangchangli <guangchangde@gmail.com>
2020-03-29 22:31:18 +0800

    fix 'thread'

--- src/main/java/com/lgc/juc/Thread_Sleep_Yield_Join.java
+++ src/main/java/com/lgc/juc/Thread_Sleep_Yield_Join.java
@@ -31,7 +31,7 @@ public class Thread_Sleep_Yield_Join {
             }
         }).start();
     }
-    // 放弃cpu 让拥有同样优先级的线程获取执行机会 状态转为就绪 等待cpu执行
+    // 放弃cpu 让拥有同样优先级的线程获取执行机会 状态转为就绪 等待cpu执行 时间片的暂时让出
     static void testYield() {
         new Thread(() -> {
             for (int i = 0; i < 5; i++) {
@@ -66,4 +66,30 @@ public class Thread_Sleep_Yield_Join {
             }
         });
     }
+    /**
+     * 对于yield方法，比较容易理解，只是简单地对于CPU时间片的“礼让”，
+     * 除非循环yield，否则一次yield，可能下次该线程仍旧可能会抢占到CPU时间片，可能方法调用和不调用没差别。
+     *
+     * sleep是静态方法，针对当前线程，进入休眠状态，两个版本的sleep方法始终有时间参数，
+     * 所以必然会在指定的时间内苏醒，他也不会释放锁，当然，sleep方法的调用非必须在同步方法（同步代码块）内。
+     *
+     * join是实例方法，表示等待谁，是用于线程顺序的调度方法，可以做到一个线程等待另外一个线程，
+     * join有三个版本，指定超时时间或者持续等待直到目标线程执行结束，join也无需在同步方法（同步代码块）内。
+     *
+     * sleep和join都是可中断方法，被其他线程中断时，都会抛出InterruptedException异常，并且会醒来。
+     *
+     * join方法底层依赖wait，对比下wait与sleep。
+     *
+     * wait和sleep都会使线程进入阻塞状态，都是可中断方法，被中断后都会抛出异常
+     *
+     * wait是Object的方法，sleep是Thread的方法
+     *
+     * wait必须在同步中执行，sleep不需要（join底层依赖wait，但是不需要在同步中，因为join方法就是synchronized的）
+     *
+     * wait会释放锁，sleep不会释放锁
+     *
+     * wait（无超时设置的版本）会持续阻塞，必须等待唤醒，而sleep必然有超时，所以一定会自己醒来
+     *
+     * wait 实例方法（Object），在对象上调用，表示在其上等待；sleep静态方法，当前线程
+     */
 }
